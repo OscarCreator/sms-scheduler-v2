d99ff9203cfd137d137b0dab024116a6fe9ea838
OscarCreator <oscar.creator13@gmail.com>
2020-09-25 15:56:43 +0200

    created basic layout for StatisticsFragment.kt

---
+++ app/src/main/java/com/oscarcreator/sms_scheduler_v2/StatisticsFragment.kt
@@ -0,0 +1,18 @@
+package com.oscarcreator.sms_scheduler_v2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StatisticsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+
+
+}
