df779eac4750f9837a64d54ff6fe92b989f28ad0
OscarCreator <oscar.creator13@gmail.com>
2020-09-25 19:25:09 +0200

    Added StatisticsFragment to top of MainActivity.

--- app/src/main/java/com/oscarcreator/sms_scheduler_v2/StatisticsFragment.kt
+++ app/src/main/java/com/oscarcreator/sms_scheduler_v2/StatisticsFragment.kt
@@ -1,10 +1,11 @@
 package com.oscarcreator.sms_scheduler_v2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.oscarcreator.sms_scheduler_v2.databinding.FragmentStatisticsBinding
 
 
 /**
@@ -12,7 +13,30 @@ import android.view.ViewGroup
  * Use the [StatisticsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+class StatisticsFragment : Fragment() {
 
+    private var _binding: FragmentStatisticsBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.tvTotalTreatments.text = "523"
+        binding.tvProcentTreatments.text = "+14% this month"
+        binding.tvTotalEarnings.text = "313800kr"
+        binding.tvTotalEarningsExclusive.text = "164745kr"
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
 }
