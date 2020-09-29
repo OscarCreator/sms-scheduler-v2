0a08a89f4370d0a28d42fa17bcad6f2b9d5f11a9
OscarCreator <oscar.creator13@gmail.com>
2020-09-29 13:46:54 +0200

    fixed table name of Customer entity to lowercase

--- app/src/main/java/com/oscarcreator/sms_scheduler_v2/data/customer/Customer.kt
+++ app/src/main/java/com/oscarcreator/sms_scheduler_v2/data/customer/Customer.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Customers")
+@Entity(tableName = "customers")
 data class Customer (
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val name: String,
