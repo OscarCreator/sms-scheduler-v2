5fcac4f733bb3cf074523928b9152e240d270f78
OscarCreator <oscar.creator13@gmail.com>
2021-01-23 16:04:42 +0100

    Title is not updated to fragment label. This Fixes #7

--- app/src/main/java/com/oscarcreator/sms_scheduler_v2/MainActivity.kt
+++ app/src/main/java/com/oscarcreator/sms_scheduler_v2/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.oscarcreator.sms_scheduler_v2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.oscarcreator.sms_scheduler_v2.data.TreatmentsStatsRepository
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.oscarcreator.sms_scheduler_v2.databinding.ActivityMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupActionBarWithNavController(
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
