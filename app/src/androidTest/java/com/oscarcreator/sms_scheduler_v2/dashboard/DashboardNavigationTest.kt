b30c5284a72ba0480b774127628f3d3d1083c5ce
OscarCreator <oscar.creator13@gmail.com>
2021-01-24 14:23:46 +0100

    Added test for navigating from dashboard to addEdit fragments with back arrow. The back button is visible because of commit 5fcac4f7.

--- app/src/androidTest/java/com/oscarcreator/sms_scheduler_v2/dashboard/DashboardNavigationTest.kt
+++ app/src/androidTest/java/com/oscarcreator/sms_scheduler_v2/dashboard/DashboardNavigationTest.kt
@@ -1,22 +1,69 @@
 package com.oscarcreator.sms_scheduler_v2.dashboard
 
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.oscarcreator.sms_scheduler_v2.MainActivity
 import com.oscarcreator.sms_scheduler_v2.R
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
 class DashboardNavigationTest {
+
+    @get: Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun navigateTo_addEditTreatment(){
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withParent(Matchers.isA(Toolbar::class.java)),
+                Matchers.isA(AppCompatTextView::class.java)))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Dashboard")))
+
+        onView(withId(R.id.fab_add_treatment))
+            .perform(ViewActions.click())
+
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withParent(Matchers.isA(Toolbar::class.java)),
+                Matchers.isA(AppCompatTextView::class.java)))
+            .check(ViewAssertions.matches(ViewMatchers.withText("AddEdit")))
+
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withParent(Matchers.isA(Toolbar::class.java)),
+                Matchers.isA(AppCompatImageButton::class.java),
+                ViewMatchers.withContentDescription("Navigate up")
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withParent(Matchers.isA(Toolbar::class.java)),
+                Matchers.isA(AppCompatTextView::class.java)))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Dashboard")))
+
+    }
 
     @Test
     fun testNavigationToAddEditTreatmentFragment() {
