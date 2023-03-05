4c22d19f4a18d1ec4fe3821c06977eef5d5dfae1
OscarCreator <oscar.creator13@gmail.com>
2023-03-05 20:43:08 +0100

    Fixed unit tests not taking whole folder

--- app/src/androidTest/java/com/oscarcreator/sms_scheduler_v2/MainActivityTest.kt
+++ app/src/androidTest/java/com/oscarcreator/sms_scheduler_v2/MainActivityTest.kt
@@ -1,8 +1,9 @@
 import android.Manifest
-import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.oscarcreator.sms_scheduler_v2.MainActivity
 import com.oscarcreator.sms_scheduler_v2.R
@@ -12,7 +13,9 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
     private lateinit var repository: ScheduledTreatmentsRepository
@@ -33,10 +36,10 @@ class MainActivityTest {
 
     @Test
     fun createScheduledTreatment() {
-        ActivityScenario.launch(MainActivity::class.java)
-
-        //navigate to add new scheduled treatment
-        onView(withId(R.id.fab_add_treatment)).perform(click())
+        launchActivity<MainActivity>().use {
+            //navigate to add new scheduled treatment
+            onView(withId(R.id.fab_add_treatment)).perform(click())
+        }
 
         //select all values
     }
